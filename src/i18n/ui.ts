8b8ec203e32336e08a8285496c74cba0105e265b
N0elung <noelb.davalos@gmail.com>
2025-12-06 13:02:19 -0800

    fix

---
+++ src/i18n/ui.ts
@@ -0,0 +1,15 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+  },
+  es: {
+    "nav.home": "Inicio",
+  },
+} as const;
